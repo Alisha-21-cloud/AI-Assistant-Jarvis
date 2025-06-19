bc5d7e21b77143b4a29b4a2b88664d0414eadf41
Alisha-21-cloud <ahmadalishasyed@gmail.com>
2025-06-19 06:24:22 +0530

    made mic btn interactive with sound

--- engine/features.py
+++ engine/features.py
@@ -1,7 +1,9 @@
 from playsound import playsound
-
+import eel
 
 # Playing assistant sound funnction
+
+@eel.expose
 def playAssistantSound():
     music_dr = "www\\assets\\audio\\start_sound.mp3"
     playsound(music_dr)
